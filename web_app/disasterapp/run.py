a78cd17c06438fe1d9e0dadfe26dc96ca52cfa49
fatinshariff <fatinshariff@gmail.com>
2021-05-01 19:59:24 +0800

    remove ../ for path

--- web_app/disasterapp/run.py
+++ web_app/disasterapp/run.py
@@ -32,11 +32,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/DisasterResponse.db')
+engine = create_engine('sqlite:///data/DisasterResponse.db')
 df = pd.read_sql_table('messages', engine)
 
 # load model
-model = joblib.load("../models/classifier.pkl")
+model = joblib.load("models/classifier.pkl")
 
 class StartingVerbExtractor(BaseEstimator, TransformerMixin):
 
@@ -170,9 +170,9 @@ def go():
     )
 
 
-#def main():
-#    app.run(host='0.0.0.0', port=3001, debug=True)
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
 
 
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    main()
